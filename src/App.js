c9c2f4247630b7d7c5bc2bde8b551a041442e26f
Lauren Whitted <lauren.whitted13@gmail.com>
2023-02-04 11:05:22 -0600

    adding counter folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  const handleClick1 = () => {
+    setCounter(counter + 1)
+  }
+  const handleClick2 = () => {
+    setCounter(counter - 1)
+  }
+  const handleClick3 = () => {
+    setCounter(counter + 5)
+  }
+  const handleClick4 = () => {
+    setCounter(counter - 5)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className= "container">
+    <div className="counter">
+    <h2>
+      Here is a counter using REACT!
+    </h2>
+    {counter}
+    <button className="buttons" onClick={handleClick1}>Increment</button>
+    <button className="buttons" onClick={handleClick2}>Decrement</button>
+    <button className="buttons" onClick={handleClick3}>ADD 5</button>
+    <button className="buttons" onClick={handleClick4}>DECREASE 5</button>
     </div>
-  );
+    </div>
+  )
+  
+  
 }
 
 export default App;
